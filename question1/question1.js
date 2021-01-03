cab055d8c1412f404f0d2e3290426d88c103f9fa
yousefaljadi <yaljadi97@outlook.com>
2021-01-03 21:41:26 +0300

    cw6
    تحياتي واشواقي للوليد

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,12 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let name1=prompt(`whats your name?`)
+console.log(name1)
+function sayhello(){
+  alert(`hello,${name1}`);
+}
+if (name1=``){
+sayhello()
+}else{
+alert(`invalid input`)
+
+}
